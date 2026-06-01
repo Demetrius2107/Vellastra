a7c4f7c4738b6b5486e15487906eb2b050f8b654
shanyuehua7 <2562017477@qq.com>
2026-06-01 15:21:44 +0800

    feat: 文章模块实体更新

--- blog-article/src/main/java/com/demetrius/blog/article/interfaces/dto/PublishAtricleRequest.java
+++ blog-article/src/main/java/com/demetrius/blog/article/interfaces/dto/PublishAtricleRequest.java
@@ -13,27 +13,31 @@ import jakarta.validation.constraints.NotBlank;
  * */
 public class PublishAtricleRequest {
 
-    /* 标题*/
+    /**
+     * 标题
+     * */
     @NotBlank(message = "标题不能为空")
     private String title;
 
-    /* 内容*/
+    /**
+     *  内容
+     *  */
     @NotBlank(message = "内容不能为空")
     private String content;
 
-    /* 总结*/
+    /** 总结*/
     private String summary;
 
-    /* 展示图*/
+    /** 展示图*/
     private String coverImage;
 
-    /* 分类ID*/
+    /** 分类ID*/
     private Long categoryId;
 
-    /* 状态 草稿 发布 删除 下架*/
+    /** 状态 草稿 发布 删除 下架*/
     private Integer status;
 
-    /* 标签*/
+    /** 标签*/
     private String tags;
 
 }
